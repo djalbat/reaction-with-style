d58b883044d729d8bdc83821e15cd4fe8eb88522
James Smith <james.smith@djalbat.com>
2022-07-14 12:24:07 +0100

    Simplified the isSubclassOf() function.

--- src/utilities/class.js
+++ src/utilities/class.js
@@ -5,17 +5,7 @@ import { React } from "reaction";
 export function isClass(argument) { return isSubclassOf(argument, React.Component); }  ///
 
 function isSubclassOf(argument, Class) {
-  let subclass = false;
+  const subclassOf = (argument.prototype instanceof Class);
 
-  if (argument.name === Class.name) {   ///
-    subclass = true;
-  } else {
-    argument = Object.getPrototypeOf(argument); ///
-
-    if (argument !== null) {
-      subclass = isSubclassOf(argument, Class);
-    }
-  }
-
-  return subclass;
+  return subclassOf;
 }
